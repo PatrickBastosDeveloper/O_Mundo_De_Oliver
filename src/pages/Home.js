f13aeb12eecb0ecfa1e3c6b5fbd922d15eeaf57f
Patrick <patrickbastosc@gmail.com>
2022-03-05 08:17:39 -0300

    doc: Created home page and css of the same

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='container-home'>
+        <p>Olá!</p>
+        <h1>Bem vindo ao Mundo De Oliver</h1>
+      </div>);
+  }
+}
+
+export default Home;
