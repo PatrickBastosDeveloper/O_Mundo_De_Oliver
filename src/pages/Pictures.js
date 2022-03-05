be3a774e4a9565ca26b0631dc673a70314f66426
Patrick <patrickbastosc@gmail.com>
2022-03-05 08:16:08 -0300

    doc: Created picture page and css

---
+++ src/pages/Pictures.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/Pictures.css';
+
+class Pictures extends Component {
+  render() {
+    return (
+      <div className='container-album'>
+        <h1>Álbum</h1>
+      </div>);
+  }
+}
+
+export default Pictures;
