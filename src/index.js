d22ce33489aa2e6f62321169169a7f0d94ca4c72
Patrick <patrickbastosc@gmail.com>
2022-03-22 11:06:36 -0300

    refact: cleaning the code

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
 
 ReactDOM.render(
-  // <Provider store={ store }> 
     <BrowserRouter>
       <App />
     </BrowserRouter>,
-  // {/* </Provider>, */}
   document.getElementById('root'),
 );
-
-// O projeto só roda se as linhas 9 e 13 estão comentadas. Está retornando a msg de erro:
-// react.development.js:1476 Uncaught Error: Invalid hook call.
