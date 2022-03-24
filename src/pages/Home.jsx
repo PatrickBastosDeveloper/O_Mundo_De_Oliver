64bbb09efb0e7fdb5e3db5cea96cf5e10c66c790
Patrick <patrickbastosc@gmail.com>
2022-03-24 06:17:16 -0300

    ci: testing global access

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import context from '../contextApi/context';
 import '../styles/Home.css';
 
 export default function Home({history}) {
+  
+  const { nameVisitants } = useContext( context )
+  console.log(nameVisitants);
+
   return (
+    
     <div className='container-home'>
-      <p>Olá!</p>
-      <h1>Bem vindo ao Mundo De Oliver</h1>
-      <button type="button" onClick={()=> history.push('/pictures')}>Minhas fotos</button>
+      <p></p>
+      <h1>{`Olááá!${ nameVisitants }`}<br/> Bem vindo ao Mundo De Oliver.</h1>
+      <button
+        type="button"
+        onClick={ () => history.push( '/pictures' ) }
+      > Minhas fotos
+      </button>
     </div>
   );
 }
